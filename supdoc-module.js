e91b5f0f16eb22d9e545d857360b9ebedc11e101
William <Wilche3@gmail.com>
2016-11-05 18:48:35 -0500

    various fixes

--- supdoc-module.js
+++ supdoc-module.js
@@ -1,9 +1,26 @@
 var supdoc = angular.module('supdoc', ['ngRoute']);
+var database = firebase.database();
 
-supdoc.config(['$locationProvider', $routeProvider,
+supdoc.config(['$locationProvider', '$routeProvider',
     function config($locationProvider, $routeProvider) {
         $routeProvider.when('/', {
             template: '<sign-in></sign-in>'.
         }). otherwise('/');
     }
 ]);
+
+supdoc.component('user' {
+    templateUrl: 'user.htm',
+    controller: function UserController($http, $location, $routeParams) {
+        //needs to know PCP
+        //needs to know doctors home screen
+        //needs to know list of doctors
+    }
+})
+
+supdoc.component('doctor') {
+    templateUrl: 'doctor.htm',
+    controller: function DoctorController()
+    //needs to have a list of patients
+    //be able to add patients
+}
