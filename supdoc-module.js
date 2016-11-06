60bc2d19e03d8f9bde7a8984a382f8df335f2961
hschirra3 <hschirra3@gatech.edu>
2016-11-05 21:20:44 -0400

    added semi colons

--- supdoc-module.js
+++ supdoc-module.js
@@ -16,11 +16,11 @@ supdoc.component('user' {
         //needs to know doctors home screen
         //needs to know list of doctors
     }
-})
+});
 
 supdoc.component('doctor' {
     templateUrl: 'doctor.htm',
     controller: function DoctorController()
     //needs to have a list of patients
     //be able to add patients
-})
+});
