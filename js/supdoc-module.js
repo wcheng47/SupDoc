ed3d48237a2e9e182f8785487d31fea6d8f2a140
William <Wilche3@gmail.com>
2016-11-05 20:26:15 -0500

    deleted copy module

--- js/supdoc-module.js
+++ js/supdoc-module.js
@@ -4,8 +4,8 @@ var database = firebase.database();
 supdoc.config(['$locationProvider', '$routeProvider',
     function config($locationProvider, $routeProvider) {
         $routeProvider.when('/', {
-            template: '<user></user>'.
-        }). otherwise('/');
+            templateUrl : 'user.htm'.
+        }).otherwise('/');
     }
 ]);
 
@@ -16,11 +16,11 @@ supdoc.component('user' {
         //needs to know doctors home screen
         //needs to know list of doctors
     }
-})
+});
 
-supdoc.component('doctor') {
+supdoc.component('doctor' {
     templateUrl: 'doctor.htm',
     controller: function DoctorController()
     //needs to have a list of patients
     //be able to add patients
-}
+});
